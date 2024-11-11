431c67b21e6b243a00e55435ada33abbe291eb37
AlonaRahilevych <ragilevic@gmail.com>
2024-11-11 20:25:46 +0100

    Create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Social from '../Social/Social';
+import styles from './Footer.module.css';
+function Footer() {
+  return (
+    <div className={styles.footer}>
+      <Social />
+    </div>
+  );
+}
+
+export default Footer;
