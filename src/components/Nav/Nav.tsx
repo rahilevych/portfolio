ea13a88ea0cac1674fd04616ec28250ab37d2e98
AlonaRahilevych <ragilevic@gmail.com>
2025-01-23 12:07:02 +0100

    update Nav component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,78 @@
+import { useState } from 'react';
 import styles from './Nav.module.css';
 function Nav() {
+  const [activeLink, setActiveLink] = useState('#home');
+
+  const handleLinkClick = (id: string) => {
+    setActiveLink(id);
+  };
+
   return (
-    <div className={styles.navbar}>
-      <li className={styles.list}>
-        <ul>Home</ul>
-        <ul>About</ul>
-        <ul>Skills</ul>
-        <ul>Projects</ul>
-        <ul>Contact</ul>
-      </li>
+    <div className={styles.wrapper}>
+      <div className='container'>
+        <div className={styles.navbar}>
+          <li className={styles.list}>
+            <ul>
+              <a
+                href='#home'
+                className={`${styles.link} ${
+                  activeLink === '#home' ? styles.active : ''
+                }`}
+                onClick={() => handleLinkClick('#home')}>
+                Home
+              </a>
+            </ul>
+            <ul>
+              <a
+                href='#about'
+                className={`${styles.link} ${
+                  activeLink === '#about' ? styles.active : ''
+                }`}
+                onClick={() => handleLinkClick('#about')}>
+                About
+              </a>
+            </ul>
+            <ul>
+              <a
+                href='#skills'
+                className={`${styles.link} ${
+                  activeLink === '#skills' ? styles.acquainted : ''
+                }`}
+                onClick={() => {
+                  handleLinkClick('#skills');
+                }}>
+                Skills
+              </a>
+            </ul>
+            <ul>
+              {' '}
+              <a
+                href='#projects'
+                className={`${styles.link} ${
+                  activeLink === 'projects' ? styles.active : ''
+                }`}
+                onClick={() => {
+                  handleLinkClick('#projects');
+                }}>
+                Projects
+              </a>{' '}
+            </ul>
+            <ul>
+              {' '}
+              <a
+                href='#contact'
+                className={`${styles.link} ${
+                  activeLink === 'contact' ? styles.active : ''
+                }`}
+                onClick={() => {
+                  handleLinkClick('#contact');
+                }}>
+                Contact
+              </a>{' '}
+            </ul>
+          </li>
+        </div>
+      </div>
     </div>
   );
 }
