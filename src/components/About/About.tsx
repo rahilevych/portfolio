cc633138198fa820a7004fcb91e2ecd383d62886
AlonaRahilevych <ragilevic@gmail.com>
2025-01-24 13:01:59 +0100

    Update styles  in About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,25 +2,19 @@ import styles from './About.module.css';
 import photo from '../../assets/img/about.jpg';
 
 const About = () => {
+  const about =
+    "I'm a Berlin-based full-stack web developer passionate about building modern, user-friendly applications. With a Bachelor's degree in Computer Science and advanced training in Full Stack Web Development with TypeScript from Code Academy Berlin, I specialize in creating scalable, efficient solutions. With knowledge of English, German, Russian, and Ukrainian, I enjoy working in diverse and collaborative teams.";
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
+    <div className={styles.wrapper} id='about'>
+      <div className='container'>
         <div className={styles.about}>
-          {' '}
-          <div className={styles.photo}>
-            <div className={styles.img}>
-              {' '}
-              <img src={photo} alt='' />
+          <h2>About Me</h2>
+          <div className={styles.info}>
+            <div className={styles.photo}>
+              <img src={photo} alt='About' />
             </div>
-          </div>
-          <div className={styles.description}>
-            <h2>About Me</h2>
-            <div className={styles.text}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-              eum, est repellat mollitia molestias vero natus hic rerum rem id
-              exercitationem veniam libero enim, consectetur atque iusto nam
-              similique dolorum!
-            </div>
+            <div className={styles.text}>{about}</div>
           </div>
         </div>
       </div>
