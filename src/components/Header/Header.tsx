5db34ef7220ec1d477075d58d79f3d3cf4df1356
AlonaRahilevych <ragilevic@gmail.com>
2025-01-24 12:56:35 +0100

    Update styles in Header plus add download cv button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,20 @@
 import styles from './Header.module.css';
-import Nav from '../Nav/Nav';
+
 import Social from '../Social/Social';
 
 const Header = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <Nav />
-        <div className={styles.title}>
-          <h1 className={styles.h1}>Alona Rahilevych</h1>
+    <div className={styles.wrapper} id='home'>
+      <div className='container'>
+        <div className={styles.header}>
+          <h1>Alona Rahilevych</h1>
           <p className={styles.p}>Full-Stack Web Developer</p>
+          <a
+            href='/docs/lebenslauf.pdf'
+            download
+            className={styles.downloadButton}>
+            Download CV
+          </a>
           <Social />
         </div>
       </div>
