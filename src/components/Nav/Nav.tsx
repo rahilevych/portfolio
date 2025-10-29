d9aacee1e0ac0e65a972741d9b6a6a8df063b864
AlonaRahilevych <ragilevic@gmail.com>
2025-10-29 22:09:48 +0100

    feat:update nav component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,100 +1,29 @@
-import { useState } from 'react';
+import { Link } from 'react-scroll';
+import { linksList } from '../../constants/constants';
 import styles from './Nav.module.css';
-function Nav() {
-  const [activeLink, setActiveLink] = useState('#home');
-  const [isMenuOpened, setIsMenuOpened] = useState(false);
 
-  const handleLinkClick = (id: string) => {
-    setActiveLink(id);
-    setIsMenuOpened(false);
-  };
-
+export const Nav = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={`container ${styles.container}`}>
-        <div className={styles.navbar}>
-          <div
-            className={`${styles.burger} ${isMenuOpened ? styles.opened : ''}`}
-            onClick={() => setIsMenuOpened(!isMenuOpened)}>
-            <span
-              className={`${styles.burgerLine} ${
-                isMenuOpened ? styles.lineTop : ''
-              }`}></span>
-            <span
-              className={`${styles.burgerLine} ${
-                isMenuOpened ? styles.lineMiddle : ''
-              }`}></span>
-            <span
-              className={`${styles.burgerLine} ${
-                isMenuOpened ? styles.lineBottom : ''
-              }`}></span>
-          </div>
-
-          <li
-            className={`${styles.list} ${!isMenuOpened ? styles.closed : ''}`}>
-            <ul>
-              <a
-                href='#home'
-                className={`${styles.link} ${
-                  activeLink === '#home' ? styles.active : ''
-                }`}
-                onClick={() => handleLinkClick('#home')}>
-                Home
-              </a>
-            </ul>
-            <ul>
-              <a
-                href='#about'
-                className={`${styles.link} ${
-                  activeLink === '#about' ? styles.active : ''
-                }`}
-                onClick={() => handleLinkClick('#about')}>
-                About
-              </a>
-            </ul>
-            <ul>
-              <a
-                href='#skills'
-                className={`${styles.link} ${
-                  activeLink === '#skills' ? styles.active : ''
-                }`}
-                onClick={() => {
-                  handleLinkClick('#skills');
-                }}>
-                Skills
-              </a>
-            </ul>
-            <ul>
-              {' '}
-              <a
-                href='#projects'
-                className={`${styles.link} ${
-                  activeLink === '#projects' ? styles.active : ''
-                }`}
-                onClick={() => {
-                  handleLinkClick('#projects');
-                }}>
-                Projects
-              </a>{' '}
-            </ul>
-            <ul>
-              {' '}
-              <a
-                href='#contact'
-                className={`${styles.link} ${
-                  activeLink === '#contact' ? styles.active : ''
-                }`}
-                onClick={() => {
-                  handleLinkClick('#contact');
-                }}>
-                Contact
-              </a>{' '}
-            </ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        {' '}
+        {linksList.map((link, index) => (
+          <li key={index}>
+            {' '}
+            <Link
+              to={link.navigateTo}
+              smooth={true}
+              duration={500}
+              spy={true}
+              offset={-50}
+              activeClass={styles.active}
+            >
+              <span className={styles.line}></span>
+              {link.title}
+            </Link>
           </li>
-        </div>
-      </div>
-    </div>
+        ))}
+      </ul>
+    </nav>
   );
-}
-
-export default Nav;
+};
