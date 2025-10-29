ba6c64b80fde5832b4936827649ce1f4c264978e
AlonaRahilevych <ragilevic@gmail.com>
2025-10-29 22:40:36 +0100

    feat: create description component

---
+++ src/components/description/Description.tsx
@@ -0,0 +1,10 @@
+import styles from './Description.module.css';
+
+export const Description = () => {
+  return (
+    <div className={styles.info}>
+      <h2>Alona Rahilevych</h2>
+      <p>Full Stack Developer</p>
+    </div>
+  );
+};
