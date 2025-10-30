6ee0bb17c23151b0149b0e4f5c754dcdbd3c6b20
AlonaRahilevych <ragilevic@gmail.com>
2025-10-30 18:13:01 +0100

    refactor:modify components

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -1,11 +1,12 @@
+import { socials } from '../../constants/constants.tsx';
 import styles from './Social.module.css';
-import { socials } from '../../constants/constants';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Social() {
   return (
     <ul className={styles.list}>
-      {socials.map((icon) => (
-        <li>
+      {socials.map((icon, index) => (
+        <li key={index}>
           {' '}
           <a href={icon.link} className={styles.link} target='_blank'>
             <FontAwesomeIcon icon={icon.icon} className={styles.icon} />
--- src/ui/title/Title.tsx
+++ src/ui/title/Title.tsx
@@ -2,7 +2,8 @@ import styles from './Title.module.css';
 
 interface TitleProps {
   text: string;
+  className?: string;
 }
-export const Title = ({ text }: TitleProps) => {
-  return <h2 className={styles.header}>{text}</h2>;
+export const Title = ({ text, className }: TitleProps) => {
+  return <h2 className={`${styles.header} ${className}`}>{text}</h2>;
 };
