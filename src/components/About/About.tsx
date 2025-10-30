f9ba8b23ac42bbbdad166cf24a8b1fa40fe7af16
AlonaRahilevych <ragilevic@gmail.com>
2025-10-30 18:14:06 +0100

    refactor:update about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,24 +1,28 @@
 import styles from './About.module.css';
-import photo from '../../assets/img/about.jpg';
+import photo from '../../assets/img/alonafo.jpg';
+import { Title } from '../../ui/title/Title';
+import { aboutParagraphs } from '../../constants/constants';
 
 const About = () => {
-  const about =
-    "I'm a Berlin-based full-stack web developer passionate about building modern, user-friendly applications. With a Bachelor's degree in Computer Science and advanced training in Full Stack Web Development with TypeScript from Code Academy Berlin, I specialize in creating scalable, efficient solutions. With knowledge of English, German, Russian, and Ukrainian, I enjoy working in diverse and collaborative teams.";
-
   return (
-    <div className='wrapper' id='about'>
-      <div className='container'>
-        <div className={styles.about}>
-          <h2>About Me</h2>
-          <div className={styles.info}>
-            <div className={styles.photo}>
-              <img src={photo} alt='About' />
+    <section className='section'>
+      <Title text='About Me' />
+      <div className={styles.info}>
+        <div className={styles.photo}>
+          <img src={photo} alt='About' />
+        </div>
+        <div className={styles.text}>
+          {' '}
+          {aboutParagraphs.map((paragraph, index) => (
+            <div key={index}>
+              {' '}
+              <p>{paragraph}</p>
+              <br />
             </div>
-            <div className={styles.text}>{about}</div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
