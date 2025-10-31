57820e3d80cb14d8ab1e9be0a76e15a707df6e44
AlonaRahilevych <ragilevic@gmail.com>
2025-10-31 15:37:06 +0100

    refactor: change screen size in useIsMobile hook

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    const handleResize = () => setIsMobile(window.innerWidth <= 1024);
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
