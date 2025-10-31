0cc29e07d695461e85bc15642af7a36033c2f93e
AlonaRahilevych <ragilevic@gmail.com>
2025-10-31 15:41:17 +0100

    styles: update about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,15 +2,33 @@ import styles from './About.module.css';
 import photo from '../../assets/img/alonafo.jpg';
 import { Title } from '../../ui/title/Title';
 import { aboutParagraphs } from '../../constants/constants';
+import { useIsMobile } from '../../hooks/useIsMobile';
+import { Description } from '../description/Description';
+import Social from '../Social/Social';
 
 const About = () => {
+  const isMobile = useIsMobile();
   return (
     <section className='section'>
+      {isMobile ? (
+        <div className={styles.mobile}>
+          {' '}
+          <Description />
+          <Social />
+        </div>
+      ) : (
+        <></>
+      )}
       <Title text='About Me' />
       <div className={styles.info}>
-        <div className={styles.photo}>
-          <img src={photo} alt='About' />
-        </div>
+        {isMobile ? (
+          <></>
+        ) : (
+          <div className={styles.photo}>
+            <img src={photo} alt='About' />
+          </div>
+        )}
+
         <div className={styles.text}>
           {' '}
           {aboutParagraphs.map((paragraph, index) => (
