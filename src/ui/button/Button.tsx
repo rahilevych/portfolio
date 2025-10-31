b3749a94217deabfb933d5c360fc822a35212fae
AlonaRahilevych <ragilevic@gmail.com>
2025-10-31 15:42:09 +0100

    styles: update styles for ui components

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -1,10 +1,11 @@
 import styles from './Button.module.css';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className?: string;
 }
-export const Button = ({ children, ...props }: ButtonProps) => {
+export const Button = ({ className, children, ...props }: ButtonProps) => {
   return (
-    <button className={styles.button} {...props}>
+    <button className={`${styles.button} ${className}`} {...props}>
       {children}
     </button>
   );
